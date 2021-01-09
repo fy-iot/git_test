ac1a3f8685c146d8f08ce381106ba720f6d19571
fengyang <fy-iot.163.com>
2021-01-09 11:18:03 +0800

    fix bug

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	printf("v1: hello, world\r\n" );
+	printf("v1: hello, world， i love you\r\n" );
 	printf("v2: how are you \r\n");
      printf("v3: how are you \r\n");
 	return 0;
