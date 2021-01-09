f9fd8d1899a213f8984b41c80345ca89958f679b
fengyang <fy-iot.163.com>
2021-01-09 11:01:04 +0800

    modified sth

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(void)
 {
 	printf("v1: hello, world\r\n" );
 	printf("v2: how are you \r\n");
-     printf("v3: how are you \r\n");
+     printf("v3: how are you， im fine \r\n");
 	return 0;
 }
